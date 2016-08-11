916f6c63f263e4e69e47da999b612329ed96e375
ynanev <furkazana@gmail.com>
2016-08-11 15:19:48 +0100

    Update MagazineObserver.java

--- src/Observer/MagazineObserver.java
+++ src/Observer/MagazineObserver.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Observer;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/** 
- *
- * @author Parfait
- */
+
 public class MagazineObserver 
 {
     private List<Magazine> magazines = new ArrayList<Magazine>();

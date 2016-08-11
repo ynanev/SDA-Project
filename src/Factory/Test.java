8cf4b082dca95e203b2a68fbe13f20b3957b2b1a
ynanev <furkazana@gmail.com>
2016-08-11 15:27:05 +0100

    Update Test.java

--- src/Factory/Test.java
+++ src/Factory/Test.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Factory;
 
 import Bridge.BlueColor;
 import Bridge.RedColor;
 
-/**
- *
- * @author Parfait
- */
+
 
 public class Test 
 {
